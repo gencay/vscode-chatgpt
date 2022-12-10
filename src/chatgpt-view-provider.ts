87d998f56f2209a806734be075c60543ec9c38d7
Ali Gençay <gencay.ali@hotmail.com>
2022-12-10 15:55:15 -0800

    Fix typos

--- src/chatgpt-view-provider.ts
+++ src/chatgpt-view-provider.ts
@@ -190,7 +190,7 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 								placeholder="Ask a question..."
 								onInput="this.parentNode.dataset.replicatedValue = this.value"></textarea>
 						</div>
-						<button class="right-8 absolute ask-button rounded-lg p-0.5 ml-5" id="ask-button">
+						<button title="Submit prompt" class="right-8 absolute ask-button rounded-lg p-0.5 ml-5" id="ask-button">
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>
 						</button>
 					</div>
