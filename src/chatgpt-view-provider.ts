9edd792c90317fe08c8953752f825c228308d46e
Ali Gençay <gencay.ali@hotmail.com>
2022-12-10 16:46:10 -0800

    Add clear and export conversations

--- src/chatgpt-view-provider.ts
+++ src/chatgpt-view-provider.ts
@@ -63,6 +63,9 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 					});
 					vscode.window.showTextDocument(document);
 					break;
+				case 'clearConversation':
+					this.prepareConversation();
+					break;
 				default:
 					break;
 			}
@@ -75,7 +78,7 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 		}
 	}
 
-	public async sendApiRequest(prompt: string, code?: string) {
+	private async prepareConversation(): Promise<boolean> {
 		this.sessionToken = await this.context.globalState.get("chatgpt-session-token") as string;
 
 		if (this.sessionToken == null) {
@@ -94,8 +97,18 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 			} catch (error: any) {
 				vscode.window.showErrorMessage("Failed to instantiate the ChatGPT API. Try ChatGPT: Clear session.", error?.message);
 				this.sendMessage({ type: 'addError' });
-				return;
+				return false;
 			}
+		}
+
+		return true;
+	}
+
+	public async sendApiRequest(prompt: string, code?: string) {
+		await this.prepareConversation();
+
+		if (this.chatGptApi == null || this.chatGptConversation == null) {
+			return;
 		}
 
 		let response: string;
@@ -157,7 +170,6 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 		const vendorMarkedJs = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'vendor', 'marked.min.js'));
 		const vendorTailwindJs = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'vendor', 'tailwindcss.3.2.4.min.js'));
 
-
 		return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
@@ -172,7 +184,40 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 			</head>
 			<body class="overflow-hidden">
 				<div class="flex flex-col h-screen">
+					<div id="introduction" class="flex h-full items-center justify-center px-6 w-full relative">
+						<div class="flex items-start text-center gap-3.5">
+							<div class="flex flex-col gap-3.5 flex-1">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="w-6 h-6 m-auto">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"></path>
+								</svg>
+								<h2 class="text-lg font-normal">Features</h2>
+								<ul class="flex flex-col gap-3.5">
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Optimized for dialogue</li>
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Improve your code, add tests & find bugs</li>
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Copy or create new files automatically</li>
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Syntax highlighting with auto language detection</li>
+								</ul>
+							</div>
+							<div class="flex flex-col gap-3.5 flex-1">
+								<svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 m-auto" height="1em" width="1em"
+									xmlns="http://www.w3.org/2000/svg">
+									<path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+									<line x1="12" y1="9" x2="12" y2="13"></line>
+									<line x1="12" y1="17" x2="12.01" y2="17"></line>
+								</svg>
+								<h2 class="text-lg font-normal">Limitations</h2>
+								<ul class="flex flex-col gap-3.5">
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">May occasionally take long time to respond/fail</li>
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">May throw HTTP 429, if you make too many requests</li>
+									<li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">If issues persist, clear your session and re-login</li>
+								</ul>
+							</div>
+						</div>
+						<p class="absolute bottom-0 max-w-sm text-center text-xs text-slate-500">Disclaimer: This extension uses unofficial ChatGPT APIs. The author is not responsible for any issues. Get your session token <a href="https://chat.openai.com">here</a>.<br /><a href="https://github.com/gencay/vscode-chatgpt">©️ Open-sourced product</a></p>
+					</div>
+
 					<div class="flex-1 overflow-y-auto" id="qa-list"></div>
+
 					<div id="in-progress" class="pl-4 pt-4 flex items-center hidden">
 						<div class="typing">Typing</div>
 						<div class="spinner">
@@ -181,6 +226,20 @@ export default class ChatGptViewProvider implements vscode.WebviewViewProvider {
 							<div class="bounce3"></div>
 						</div>
 					</div>
+
+					<div id="chat-button-wrapper" class="w-full flex gap-2 justify-center items-center hidden">
+						<button class="flex gap-2 justify-center items-center rounded-lg p-2 m-2" id="clear-button">
+							<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>
+							Clear conversation
+						</button>
+						<button class="flex gap-2 justify-center items-center rounded-lg p-2 m-2" id="export-button">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+							</svg>
+							Export all
+						</button>
+					</div>
+
 					<div class="p-4 flex items-center">
 						<div class="flex-1 textarea-wrapper">
 							<textarea
