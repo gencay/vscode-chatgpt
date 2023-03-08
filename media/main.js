d01b7b4aa4cc24f5165353b0cbe261a712f362b6
lin52025 <87741822+lin52025-iqy@users.noreply.github.com>
2023-03-08 14:48:16 +0800

    Update main.js
    To fix: https://github.com/gencay/vscode-chatgpt/issues/114

--- media/main.js
+++ media/main.js
@@ -191,7 +191,7 @@
     };
 
     document.getElementById('question-input').addEventListener("keydown", function (event) {
-        if (event.key == "Enter" && !event.shiftKey) {
+        if (event.key == "Enter" && !event.isComposing && !event.shiftKey) {
             event.preventDefault();
             addFreeTextQuestion();
         }
